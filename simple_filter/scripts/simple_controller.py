7feba5862ce588c4e46f63444fe0ff5528a8a23b
paulruvolo <paullundyruvolo@gmail.com>
2017-02-02 01:20:52 -0500

    added simple filter for day 6

---
+++ simple_filter/scripts/simple_controller.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import rospy
+import cv2
+from simple_filter.msg import VelocitySimple
+
+class Teleop(object):
+    def __init__(self):
+        rospy.init_node('teleop')
+        self.pub = rospy.Publisher('/simple_vel', VelocitySimple, queue_size=10)
+        cv2.namedWindow('main_window')
+
+    def run(self):
+        while not rospy.is_shutdown():
+            key = cv2.waitKey(10) & 0xFF
+            if key != 0xFF:
+                print key
+            if key == 83:
+                self.pub.publish(VelocitySimple(south_to_north_velocity=1))
+            elif key == 81:
+                self.pub.publish(VelocitySimple(south_to_north_velocity=-1))
+
+if __name__ == '__main__':
+    node = Teleop()
+    node.run()
