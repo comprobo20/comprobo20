43022efc560c4c53a111c30e230897dabe852406
Paul Ruvolo <paullundyruvolo@gmail.com>
2020-06-02 00:41:29 -0400

    made more flexible

--- com_workshop/matlab_examples/buildCompoundObject.m
+++ com_workshop/matlab_examples/buildCompoundObject.m
@@ -1,4 +1,12 @@
-function buildCompoundObject()
+% TODO: 
+% Do a game where you have a bunch of shapes and a bunch of posts.  You can
+% pause physics, shift the positions until they get to where you want, and
+% then unpause and see what happens!
+
+function buildCompoundObject(usePolyline)
+if nargin < 1
+    usePolyline = false;
+end
 doc = com.mathworks.xml.XMLUtils.createDocument('robot');
 robotElem = doc.getDocumentElement();
 robotElem.setAttribute('name','base');
@@ -29,7 +37,9 @@ for i = 1 : size(weights,1)
     connectWithFixedJoint(doc, ['weight_',num2str(i),'_joint'], 'sheet', ['weight_',num2str(i)], weights(i,1), weights(i,2), sheetThickness);
 end
 
-spawnModel('sheet',xmlwrite(robotElem),0,0,1);
+if ~usePolyline
+    spawnModel('sheet',xmlwrite(robotElem),0,0,1);
+end
 
 doc = com.mathworks.xml.XMLUtils.createDocument('sdf');
 sdfElem = doc.getDocumentElement();
@@ -39,13 +49,14 @@ sdfElem.appendChild(modelElem);
 modelElem.setAttribute('name','testpoly');
 
 xs = linspace(-1, 1, 50);
-%ys = abs(xs).^8;
-ys = exp(-(2*xs).^2);
+ys = abs(xs).^2;
+%ys = exp(-(2*xs).^2);
 points = [xs' ys'; xs(1) ys(1)];
-polyline(doc, modelElem, 'square', 1, 0.5, points);
+polyline(doc, modelElem, 'square', 1, 0.05, points);
 
-spawnModel('testpoly',xmlwrite(sdfElem),0,0,1,true);
-
+if usePolyline
+    spawnModel('testpoly',xmlwrite(sdfElem),0,-0.6,1,true);
+end
     function redBox(doc, name, mass, l, w, h)
         box = doc.createElement('link');
         doc.getDocumentElement().appendChild(box);
